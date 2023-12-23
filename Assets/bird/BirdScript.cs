cfd7c5f736f023a5f1188800935b9e35fda4d893
rafael <rafa.viana.moc@gmail.com>
2023-12-23 11:24:07 -0300

    animate rotation

--- Assets/bird/BirdScript.cs
+++ Assets/bird/BirdScript.cs
@@ -7,17 +7,25 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D birdRigidBody;
     public float jumpForce = 10;
     private LogicManager logic;
+    private bool isRotating = false; // To check if rotation is in progress
 
     void Start()
     {
-        logic = GameObject.FindGameObjectWithTag("Logic")
-                    .GetComponent<LogicManager>();
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
     }
 
     void Update()
     {
-        if(!logic.isGameOver && Input.GetKeyDown(KeyCode.Space))
-            birdRigidBody.velocity = Vector2.up * jumpForce;
+        if (!logic.isGameOver && Input.GetKeyDown(KeyCode.Space))
+            jump();
+
+        if (transform.position.y <= -21 || transform.position.y >= 21)
+            logic.gameOver();
+
+        if (isRotating)
+        {
+            RotateBack();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,4 +33,21 @@ public class BirdScript : MonoBehaviour
         logic.gameOver();
     }
 
+    private void jump()
+    {
+        birdRigidBody.velocity = Vector2.up * jumpForce;
+        transform.rotation = Quaternion.Euler(0, 0, 45);
+        isRotating = true;
+    }
+
+    private void RotateBack()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * 5);
+
+        if (Quaternion.Angle(transform.rotation, Quaternion.Euler(0, 0, 0)) < 1)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            isRotating = false;
+        }
+    }
 }

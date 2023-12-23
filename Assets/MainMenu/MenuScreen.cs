435b54a98ad1cd01d5cc51331f9b9a15192b7ca4
rafael <rafa.viana.moc@gmail.com>
2023-12-23 15:02:31 -0300

    menu done!

--- Assets/MainMenu/MenuScreen.cs
+++ Assets/MainMenu/MenuScreen.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScreen : MonoBehaviour
 {
     public Image cloudTop, cloudBottom;
+    public Button playBtn, aboutBtn;
+
     [SerializeField] private float cloudSpeed = 100f;
     private const float ResetPosition = 1345f;
     
     void Start()
     {
-        if(cloudTop == null || cloudBottom == null)
+        if (cloudTop == null || cloudBottom == null)
             Debug.LogError("Cloud objects not found. Make sure your tags are set correctly.");
 
         Debug.Log("MenuScreen started. Clouds initialized." + cloudTop.tag + cloudBottom.tag);
+
+        if (playBtn != null)
+            playBtn.onClick.AddListener(startGame);
+        else
+            Debug.LogError("Play button not assigned.");
+
+        if (aboutBtn != null)
+            aboutBtn.onClick.AddListener(OpenAboutPage);
+        else
+            Debug.LogError("About button not assigned.");
     }
 
     void Update()
@@ -34,5 +47,16 @@ public class MenuScreen : MonoBehaviour
 
         Debug.Log(cloud.name + " position: " + position.x);
         cloud.transform.position = position;
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenAboutPage()
+    {
+    
+        Application.OpenURL("https://github.com/rafael-allves");
     }
 }
